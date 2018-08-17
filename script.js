20ea2d527cb521e1f59acb211e0d27524733aac1
marcus290 <36244342+marcus290@users.noreply.github.com>
2018-08-17 13:21:42 +1200

    CSS applied

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function getHome() {
+    menuClose();
     const version_d = document.getElementById("show_result");
     version_d.innerHTML = "<h1>Welcome to the Department of Computer Science</h1><p>Welcome to New Zealand's leading computer science department. We pride ourselves on the excellence of our staff and our students.</p>";
 }
 
 function getCourses() {
+    menuClose();
     const xhr = new XMLHttpRequest(); 
     const uri = "http://redsox.uoa.auckland.ac.nz/ups/UniProxService.svc/courses"; 
     xhr.open("GET", uri, true); 
@@ -18,7 +20,7 @@ function getCourses() {
             htmlToInsert += '<div class="courses"><h2>COMPSCI ' + courseNum + '</h2>' +
                             '<h3 id="' + courseNum + 'title"></h3>' +
                             '<p id="' + courseNum + 'des"></p>' +
-                            '<a onclick="getTimetable(' + courseNum + ')">Timetable</a>' +
+                            '<a class="timetable" onclick="getTimetable(' + courseNum + ')">Current course timetable</a>' +
                             '<div class="timetable" id="' + courseNum + 'timetable"></div>' +'</div>'; 
         });
         version_d.innerHTML = htmlToInsert;
@@ -43,14 +45,25 @@ function getTimetable(catalogNumber) {
             const timetableData = JSON.parse(xhr.responseText).data;
             const version_d = document.getElementById(catalogNumber + "timetable");
             let htmlToInsert = '';
+            const today = new Date();
             for (let i = 0; i < timetableData.length; i++) {
-                htmlToInsert += timetableData[i].component + '<br/>';
+                let periodSD = new Date(timetableData[i].startDate);
+                let periodED = new Date(timetableData[i].endDate);
+                if (today < periodSD || today > periodED) {continue;}
+                htmlToInsert += '<br/>' + timetableData[i].component + '<br/>';
                 for (let j = 0; j < timetableData[i].meetingPatterns.length; j++) {
-                    htmlToInsert += timetableData[i].meetingPatterns[j].daysOfWeek +
-                                    timetableData[i].meetingPatterns[j].location +
-                                    timetableData[i].meetingPatterns[j].startTime +
-                                    timetableData[i].meetingPatterns[j].endTime + '<br/>';
+                    let startDate = new Date(timetableData[i].meetingPatterns[j].startDate);
+                    let endDate = new Date(timetableData[i].meetingPatterns[j].endDate);
+                    if (today < startDate || today > endDate) {continue;}
+
+                    htmlToInsert += timetableData[i].meetingPatterns[j].daysOfWeek.replace(/^\w/, ch => ch.toUpperCase()) + ' ' +
+                                    (timetableData[i].meetingPatterns[j].startTime.slice(0, 5)) + '-' +
+                                    (timetableData[i].meetingPatterns[j].endTime.slice(0, 5)) + 
+                                    ' Location: ' + timetableData[i].meetingPatterns[j].location +
+                                    '<br/>';
                 }
+            if (htmlToInsert === '') {htmlToInsert = "None scheduled currently";}
+            console.log(htmlToInsert);
             version_d.innerHTML = htmlToInsert;
             }
         }
@@ -61,6 +74,7 @@ function getTimetable(catalogNumber) {
 }
 
 function getPeople() { 
+    menuClose();
     const xhr = new XMLHttpRequest(); 
     const uri = "http://redsox.uoa.auckland.ac.nz/ups/UniProxService.svc/people"; 
     xhr.open("GET", uri, true); 
@@ -70,15 +84,15 @@ function getPeople() {
         let htmlToInsert = '<h1>People</h1>'; 
         for (let i = 0; i < peopleData.length; i++) {
             const uid = peopleData[i].profileUrl[1];
-            htmlToInsert += '<div class="people">' + peopleData[i].firstname + " " + peopleData[i].lastname + 
-                            '<br/>Alt name: <span id="' + uid + 'altname"></span>' +
+            htmlToInsert += '<div class="people">' + (
+                                (peopleData[i].hasOwnProperty('imageId')) ? 
+                                ('<img class="profile" src="https://unidirectory.auckland.ac.nz/people/imageraw/' + uid + '/' + peopleData[i].imageId + '/small">') :
+                                ('<div class="profile-filler"></div>')
+                            ) + '<span class="pname">' + peopleData[i].firstname + " " + peopleData[i].lastname + '</span>' +
+                            '<br/>' + peopleData[i].jobtitles[0] +
                             '<br/>Email: ' + ('<a href="mailto:' + peopleData[i].emailAddresses + '">' + peopleData[i].emailAddresses + '</a>') +
                             '<br/>Phone: <span id="' + uid + 'phone"></span>' +
-                            '<br/><a href="http://redsox.uoa.auckland.ac.nz/ups/UniProxService.svc/vcard?u=' + uid + '">vCard</a>' + (
-                                (peopleData[i].hasOwnProperty('imageId')) ? 
-                                ('<img src="https://unidirectory.auckland.ac.nz/people/imageraw/' + uid + '/' + peopleData[i].imageId + '/small">') :
-                                ('<div class="filler-img"></div>')
-                            ) + '</div>';
+                            '<br/><a href="http://redsox.uoa.auckland.ac.nz/ups/UniProxService.svc/vcard?u=' + uid + '">vCard</a>' + '</div>';
         }
         version_d.innerHTML = htmlToInsert;
         for (let i = 0; i < peopleData.length; i++) {
@@ -88,14 +102,13 @@ function getPeople() {
             phonexhr.open("GET", phoneuri, true);
             phonexhr.onload = () => {
                 const personData = JSON.parse(phonexhr.responseText);
-                document.getElementById(uid+ "altname").innerHTML = personData.fullName;
                 let phoneHtml;
                 if (personData.phoneNumbers.length > 0) {
                     phoneHtml = ('<a href="tel:' + personData.phoneNumbers[0].phone + '">' + personData.phoneNumbers[0].phone + '</a>');
                 } else {
                     phoneHtml = "n/a";
                 }
-                document.getElementById(uid+ "phone").innerHTML = phoneHtml;
+                document.getElementById(uid + "phone").innerHTML = phoneHtml;
             }
             phonexhr.send(null);
         }
@@ -105,6 +118,7 @@ function getPeople() {
 }
 
 function getNews() {
+    menuClose();
     const xhr = new XMLHttpRequest(); 
     const uri = "http://redsox.uoa.auckland.ac.nz/ups/UniProxService.svc/news"; 
     xhr.open("GET", uri, true);
@@ -124,6 +138,7 @@ function getNews() {
 }
 
 function getNotices() {
+    menuClose();
     const xhr = new XMLHttpRequest(); 
     const uri = "http://redsox.uoa.auckland.ac.nz/ups/UniProxService.svc/notices"; 
     xhr.open("GET", uri, true);
@@ -142,35 +157,44 @@ function getNotices() {
     xhr.send(null);
 }
 
+function postComments(form) {
+    const formEle = form.elements;
+    //console.log(formEle.name.value, formEle.message.value);
+    const xhr = new XMLHttpRequest(); 
+    const uri = "http://redsox.uoa.auckland.ac.nz/ups/UniProxService.svc/comment?name=" + form.elements.name.value; 
+    xhr.open("POST", uri, true); 
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8"); 
+    xhr.onload = () => { 
+        getComments();
+    }
+    let objectToPost = form.elements.message.value;
+    xhr.send(JSON.stringify(objectToPost));
+}
 function getComments() {
+    menuClose();
     const xhr = new XMLHttpRequest(); 
     const uri = "http://redsox.uoa.auckland.ac.nz/ups/UniProxService.svc/htmlcomments"; 
     xhr.open("GET", uri, true);
     xhr.onload = () => {
         const version_d = document.getElementById("show_result");
         let htmlToInsert = '<form id="myForm"><textarea name="message" rows="4" cols="50" placeholder="Comment"></textarea><br/>' + 
-                        '<label for="commentName">Name:</label><input id="myName" name="name"><input type="submit" value="Submit"></form>';
+                        '<label for="commentName">Name:</label><input id="myName" name="name"><input type="submit" value="Submit"></form>' +
+                        '<div id="messageSent"></div>';
         version_d.innerHTML = htmlToInsert + xhr.responseText;
         const form = document.getElementById("myForm");
         form.addEventListener("submit", function (event) {
             event.preventDefault();
             postComments(form);
-            getComments();
         });
     } 
     xhr.send(null);
 }
-function postComments(form) {
-    const formEle = form.elements;
-    console.log(formEle.name.value, formEle.message.value);
-    const xhr = new XMLHttpRequest(); 
-    const uri = "http://redsox.uoa.auckland.ac.nz/ups/UniProxService.svc/comment?name=" + form.elements.name.value; 
-    xhr.open("POST", uri, true); 
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8"); 
-    xhr.onload = () => { 
-        ;
-    }
-    let objectToPost = form.elements.message.value;
-    xhr.send(JSON.stringify(objectToPost));
-    
+
+function menuOpen() {
+    document.getElementById("menu").style.height = "auto";
+    document.getElementById("menuButton").setAttribute('onclick', "menuClose()");
+}
+function menuClose() {
+    document.getElementById("menu").style.height = "0";
+    document.getElementById("menuButton").setAttribute('onclick', "menuOpen()");
 }
